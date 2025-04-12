ab51b482fa5e217bffd545e33b5ce60b13d398cc
HarishPracticeGit <harish.harsha.in@outlook.com>
2025-04-12 21:24:37 +0530

    new vent added

--- prod/vnet.tf
+++ prod/vnet.tf
@@ -20,3 +20,25 @@ module "prod_vnet_east" {
     }
   ]
 }
+
+module "prod_vnet_east2" {
+  source              = "../modules/network"
+  vnet_name           = "vnet1-east-prod"
+  address_space       = ["10.50.0.0/16"]
+  location            = "East US"
+  resource_group_name = "sanofi-prod-rg"
+  tags = {
+    environment = "prod"
+  }
+  subnets = [
+    {
+      name             = "prod1-subnet1"
+      address_prefixes = ["10.50.1.0/24"]
+    },
+    {
+      name             = "prod2-subnet2"
+      address_prefixes = ["10.50.2.0/24"]
+      service_endpoints = ["Microsoft.Storage"]
+    }
+  ]
+}
