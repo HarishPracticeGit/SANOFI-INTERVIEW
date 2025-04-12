bf249dfa1a8ff791a9dd3b9a9ae255a45c5a8104
HarishPracticeGit <harish.harsha.in@outlook.com>
2025-04-12 20:32:31 +0530

    vm and vnet files modified

--- prod/vm.tf
+++ prod/vm.tf
@@ -4,7 +4,7 @@ module "prod-vm2" {
   vm_name             = "vm-east-1"
   location            = "East US"
   resource_group_name = "sanofi-prod-rg"
-  subnet_id           = module.vnet_west.subnet_ids["backend"]
+  subnet_id           = module.prod_vnet_east.subnet_ids["prod-subnet2"]
   admin_username      = data.azurerm_key_vault_secret.admin_username.value
   admin_password      = data.azurerm_key_vault_secret.admin_password.value
   create_public_ip    = true
