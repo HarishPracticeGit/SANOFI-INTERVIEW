1c4816189cf850e7d2c57e49663a00b8140f6671
HarishPracticeGit <83857370+HarishPracticeGit@users.noreply.github.com>
2025-04-14 12:02:31 +0530

    Update data.tf

--- prod/data.tf
+++ prod/data.tf
@@ -1,16 +1,16 @@
-# calling existing key vault
+# calling existing key vault details
 data "azurerm_key_vault" "main" {
   name                = "sanofi-keyvault"
   resource_group_name = "sanofi-dev-rg"
 }
 
-# calling existing key vault secret
+# calling existing key vault secret details
 data "azurerm_key_vault_secret" "admin_username" {
   name         = "vm-admin-username"
   key_vault_id = data.azurerm_key_vault.main.id
 }
 
-# calling existing key vault secret
+# calling existing key vault secret details
 data "azurerm_key_vault_secret" "admin_password" {
   name         = "vm-admin-password"
   key_vault_id = data.azurerm_key_vault.main.id
